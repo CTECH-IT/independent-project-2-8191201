b2fea5e66d97d79613430d69b59e8bbf5d401b69
mhuang03 <huangmichael150@gmail.com>
2022-03-11 10:05:02 -0600

    um website made

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
-// Put your JavaScript here
+function addLogoButton() {
+    "use strict";
+    let logo = document.querySelector(".logo");
+    console.log(logo);
+    logo.addEventListener("click", function() {
+        console.log("clicked logo");
+        this.classList.add("clicked");
+        console.log(this);
+        setTimeout(function(logo) {
+            logo.classList.remove("clicked");
+            console.log(logo);
+        }.bind(null, this), 1000);
+    });
+}
+
+function initialize() {
+    "use strict";
+    addLogoButton();
+}
+
+initialize();
